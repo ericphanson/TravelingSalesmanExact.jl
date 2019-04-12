215f338d6ea4769321238b641a7d054e79747da0
Eric <5846501+ericphanson@users.noreply.github.com>
2019-04-12 20:34:01 +0100

    Fix path to allow running via include

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using TSP_MIP
 using Test
 
 @testset "att48.tsp" begin
-    cities = simple_parse_tsp("att48.tsp")
+    cities = simple_parse_tsp(joinpath(@__DIR__, "att48.tsp"))
     result = get_optimal_tour(cities; distance = TSP_MIP.ATT)
     @test result.cost ≈ 10628
 end
