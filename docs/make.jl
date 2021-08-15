b8690bc1be94db2b2b79ed522286e62f5e3644e0
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-08-15 17:50:50 +0200

    fix caching

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,6 @@ asciinema = joinpath(Conda.python_dir(env), "asciinema")
 # need to rebuild them unless the code changes.
 const ASCIINEMA_CAST_DIR = get_scratch!(TravelingSalesmanExact, "gifs")
 
-
 # We create an asciinema config
 # so that we can start Julia in quiet mode
 # and clear the screen before starting.
@@ -28,25 +27,25 @@ open(joinpath(ASCIINEMA_CONFIG_DIR, "config"), write=true) do io
     """)
 end
 
-function record(commands, name)
+function record(commands, path)
     io = IOBuffer()
     write(io, commands)
     # Make sure we press enter after all the commands
     endswith(commands, '\n') || write(io, '\n')
     write(io, 0x4) # write ctrl-d to exit the process and end the cast
     seekstart(io)
-    run(pipeline(addenv(`$asciinema rec $(name).cast --overwrite`, "ASCIINEMA_CONFIG_HOME" => ASCIINEMA_CONFIG_DIR,
+    run(pipeline(addenv(`$asciinema rec $path --overwrite`, "ASCIINEMA_CONFIG_HOME" => ASCIINEMA_CONFIG_DIR,
     "JULIA_PROJECT" => Base.active_project()); stdin=io))
     return nothing
 end
 
 macro gif_str(commands)
     # We use a hash of the commands to name the cast
-    name  = string(uuid5(UUID("faedbbc1-c8d1-4dfe-aa46-8b80407bf145"), commands))
+    filename = string(uuid5(UUID("faedbbc1-c8d1-4dfe-aa46-8b80407bf145"), commands), ".cast")
 
-    path = joinpath(ASCIINEMA_CAST_DIR, name)
+    path = joinpath(ASCIINEMA_CAST_DIR, filename)
     isfile(path) || record(commands, path)
-    relative_path = "./assets/gifs/$(name).cast"
+    relative_path = "./assets/gifs/$filename"
     return HTML("""<asciinema-player src="$relative_path" idle-time-limit="2" autoplay="true" start-at="0.25"></asciinema-player >""")
 end
 
