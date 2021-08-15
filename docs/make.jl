4630004f97d124a002e5570192c63e21ba684f08
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-08-15 18:28:23 +0200

    rm debug code

--- docs/make.jl
+++ docs/make.jl
@@ -71,16 +71,6 @@ macro gif_str(commands)
 end
 
 
-gif"""
-using TravelingSalesmanExact, GLPK
-set_default_optimizer!(GLPK.Optimizer)
-n = 50
-cities = [ 100*rand(2) for _ in 1:n];
-tour, cost = get_optimal_tour(cities; verbose = true)
-plot_cities(cities[tour])
-"""
-
-
 
 makedocs(;
     modules=[TravelingSalesmanExact],
